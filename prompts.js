657c33180bdc23692fd58fdd260d382416c5fd1a
daemon <alsoankit@gmail.com>
2024-12-24 16:10:07 +0530

    test commit

--- prompts.js
+++ prompts.js
@@ -4,6 +4,10 @@ let details = {
   name: "Ankit",
   roll: 23051409,
   roll: 23051409,
+  roll: 23051409,
+  roll: 23051409,
+  section: "cse35",
+  section: "cse35",
   section: "cse35",
   section: "cse35",
 }
